73512036ecd2a495d8d50a766bccf49092da826e
Diana <kosyanenkodiana@yandex.ru>
2022-12-09 18:03:34 +0300

    Add new rep

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿/*
+Console.WriteLine("Enter your number");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter your number");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+{
+    Console.WriteLine("a > b");
+}
+else
+{
+    Console.WriteLine("b > a");
+}*/
+/*
+Console.WriteLine("Enter your number");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter your number");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter your number");
+int c = Convert.ToInt32(Console.ReadLine());
+if (b < a && a > c)
+{
+    Console.WriteLine("a");
+}
+else if (a < b && b > c)
+{
+    Console.WriteLine("b");
+}
+else if (c < a && c > b)
+{
+    Console.WriteLine("c");
+}*/
+/*Console.WriteLine("Enter your number");
+int numb = Convert.ToInt32(Console.ReadLine());
+if (numb % 2 == 0)
+{
+    Console.WriteLine("Четное");
+}
+else
+{
+    Console.WriteLine("Не четное");
+}*/
+/*int i = 1;
+bool not = true;
+
+Console.WriteLine("Enter your number");
+int N = Convert.ToInt32(Console.ReadLine());
+while (i <= N)
+{
+    if (i % 2 != 1)
+    {
+       Console.WriteLine(i + ", ");
+       not = false;
+    }
+    i++;
+}
+
+if (not)
+{
+   Console.WriteLine("Нет четных чисел");
+}*/
